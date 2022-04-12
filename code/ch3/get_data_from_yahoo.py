e2c40069175895fb6abce991d7ffef1eb41b29e4
eyelash94500 <eyelash.94500@gmail.com>
2022-04-12 19:38:33 +0800

    V1.2 Move the files

--- code/ch3/get_data_from_yahoo.py
+++ code/ch3/get_data_from_yahoo.py
@@ -2,5 +2,5 @@ import requests
 
 address = "https://query1.finance.yahoo.com/v8/finance/chart/2317.TW?period1=0&period2=1599926400&interval=1d&events=history&=hP2rOschxO0"
 
-response = requests.get(address)
+response = requests.get(address, headers={"User-agent": ""})
 print(response.text)
